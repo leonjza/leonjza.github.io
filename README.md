b97a3e59e68664d8aad305ef0a3ed320ae4d234b
Leon Jacobs <leonja511@gmail.com>
2016-01-09 10:03:01 +0200

    Updating Blog Source

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # leonjza.github.io
 
 A checkbox Uncheckers' Notepad
+Setup on a new computer with:
+
+```bash
+curl https://raw.githubusercontent.com/leonjza/leonjza.github.io/source/setup.sh | bash
+```
--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 # Deploy a hugo generated site!
 # @leonjza - 2016
 
+set -e
+
 # Update local source with remote
 echo -e "\033[0;32mUpdating local copy of project sources..\033[0m"
 git pull origin source
